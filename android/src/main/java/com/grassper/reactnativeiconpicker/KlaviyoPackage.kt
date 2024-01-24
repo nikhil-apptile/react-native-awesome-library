855735ec2840edbd49984da50ebc21a89cd719c6
Nikhil Yadav <nikhil.yadav.nm@apptile.io>
2024-01-24 18:25:19 +0530

    fixed KlaviyoPackage.kt

--- android/src/main/java/com/grassper/reactnativeiconpicker/KlaviyoPackage.kt
+++ android/src/main/java/com/grassper/reactnativeiconpicker/KlaviyoPackage.kt
@@ -1,14 +1,21 @@
 package com.grassper.reactnativeiconpicker
 
+import android.view.View
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
+import java.util.Collections
 
 class KlaviyoPackage : ReactPackage {
 
     override fun createNativeModules(reactContext: ReactApplicationContext):
             MutableList<NativeModule> {
         return mutableListOf(KlaviyoModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> {
+        return Collections.emptyList()
     }
 }
