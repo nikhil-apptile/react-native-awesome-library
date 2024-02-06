a2d7e4684ac078b0af34a95049e158db9a838ae4
Nikhil Yadav <nikhil.yadav.nm@apptile.io>
2024-02-06 17:17:41 +0530

    import foundation

--- ios/KlaviyoModule.m
+++ ios/KlaviyoModule.m
@@ -1,33 +1,45 @@
-//
-//  IconPickerModule.m
-//  IconPickerModule
-//
-//  Copyright © 2021 grassper. All rights reserved.
-//
+// //
+// //  IconPickerModule.m
+// //  IconPickerModule
+// //
+// //  Copyright © 2021 grassper. All rights reserved.
+// //
 
-// #import <React/RCTBridgeModule.h>
-#import "KlaviyoModule.h"
+// // #import <React/RCTBridgeModule.h>
+// #import "KlaviyoModule.h"
 
-// @interface RCT_EXTERN_MODULE(KlaviyoModule, NSObject)
+// // @interface RCT_EXTERN_MODULE(KlaviyoModule, NSObject)
 
-// RCT_EXTERN_METHOD(initializeKlaviyoSDK:(NSString *)value)
-// RCT_EXTERN_METHOD(setExternalId:(NSString *)value)
-// // RCT_EXTERN_METHOD(getExternalId:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-// RCT_EXTERN_METHOD(getExternalId:(RCTResponseSenderBlock)callback)
-// RCT_EXTERN_METHOD(setPushToken:(NSData *)tokenData)
-// RCT_EXTERN_METHOD(setPushTokenNative:(NSData *)tokenData)
+// // RCT_EXTERN_METHOD(initializeKlaviyoSDK:(NSString *)value)
+// // RCT_EXTERN_METHOD(setExternalId:(NSString *)value)
+// // // RCT_EXTERN_METHOD(getExternalId:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+// // RCT_EXTERN_METHOD(getExternalId:(RCTResponseSenderBlock)callback)
+// // RCT_EXTERN_METHOD(setPushToken:(NSData *)tokenData)
+// // RCT_EXTERN_METHOD(setPushTokenNative:(NSData *)tokenData)
 
-// @end
+// // @end
 
-@implementation KlaviyoModule
-RCT_EXPORT_MODULE();
+// @implementation KlaviyoModule
+// RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(initializeKlaviyoSDK: (NSString *)apiKey)
-{
-    [KlaviyoModule initializeKlaviyoSDK: apiKey];
-}
+// RCT_EXPORT_METHOD(initializeKlaviyoSDK: (NSString *)apiKey)
+// {
+//     [KlaviyoModule initializeKlaviyoSDK: apiKey];
+// }
 
-RCT_EXPORT_METHOD(setExternalId: (NSString *)externalId)
-{
-    [KlaviyoModule setExternalId: externalId];
-}
+// RCT_EXPORT_METHOD(setExternalId: (NSString *)externalId)
+// {
+//     [KlaviyoModule setExternalId: externalId];
+// }
+
+
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(KlaviyoModule, NSObject)
+
+RCT_EXTERN_METHOD(initializeKlaviyoSDK:(NSString *)apiKey)
+RCT_EXTERN_METHOD(setExternalId:(NSString *)externalId)
+RCT_EXTERN_METHOD(getExternalId:(RCTResponseSenderBlock)callback)
+RCT_EXTERN_METHOD(setPushToken:(NSData *)tokenData)
+
+@end
--- ios/KlaviyoModule.swift
+++ ios/KlaviyoModule.swift
@@ -1,54 +1,86 @@
-//
-//  KlaviyoModule.swift
-//  KlaviyoModule
-//
-//  Copyright © 2021 grassper. All rights reserved.
-//
+// //
+// //  KlaviyoModule.swift
+// //  KlaviyoModule
+// //
+// //  Copyright © 2021 grassper. All rights reserved.
+// //
 
+// import KlaviyoSwift
+
+// @objc(KlaviyoModule)
+// class KlaviyoModule: NSObject {
+//   @objc
+//   public static func initializeKlaviyoSDK(_ value: String) {
+//     KlaviyoSDK().initialize(with: value)
+//   }
+
+//   @objc
+//   public static func setExternalId(_ value: String) {
+//     KlaviyoSDK().set(externalId: value)
+//   }
+
+//   // @objc
+//   // public static func getExternalId() -> String {
+//   //   return KlaviyoSDK().externalId ?? ""
+//   // }
+
+//   // @objc(getExternalId:rejecter:)
+//   // func getExternalId(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) {
+//   //   if let externalId = KlaviyoSDK().externalId {
+//   //     resolve(externalId)
+//   //   } else {
+//   //     reject("no_id", "There is no external ID set", nil)
+//   //   }
+//   // }
+
+//   @objc(getExternalId:)
+//   public static func getExternalId(callback: RCTResponseSenderBlock) {
+//     if let externalId = KlaviyoSDK().externalId {
+//       callback([NSNull(), externalId])
+//     } else {
+//       callback(["External ID not found", NSNull()])
+//     }
+//   }
+
+//   @objc
+//   func setPushToken(tokenData: Data) {
+//     KlaviyoSDK().setPushToken(tokenData)
+//   }
+
+//   @objc
+//   class func setPushTokenNative(tokenData: Data) {
+//     KlaviyoSDK().setPushToken(tokenData)
+//   }
+// }
+
+
+
+import Foundation
 import KlaviyoSwift
 
 @objc(KlaviyoModule)
 class KlaviyoModule: NSObject {
-  @objc
-  public static func initializeKlaviyoSDK(_ value: String) {
-    KlaviyoSDK().initialize(with: value)
-  }
-
-  @objc
-  public static func setExternalId(_ value: String) {
-    KlaviyoSDK().set(externalId: value)
-  }
-
-  // @objc
-  // public static func getExternalId() -> String {
-  //   return KlaviyoSDK().externalId ?? ""
-  // }
-
-  // @objc(getExternalId:rejecter:)
-  // func getExternalId(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) {
-  //   if let externalId = KlaviyoSDK().externalId {
-  //     resolve(externalId)
-  //   } else {
-  //     reject("no_id", "There is no external ID set", nil)
-  //   }
-  // }
-
-  @objc(getExternalId:)
-  public static func getExternalId(callback: RCTResponseSenderBlock) {
-    if let externalId = KlaviyoSDK().externalId {
-      callback([NSNull(), externalId])
-    } else {
-      callback(["External ID not found", NSNull()])
+    @objc
+    static func initializeKlaviyoSDK(_ apiKey: String) {
+        KlaviyoSDK.initialize(with: apiKey)
     }
-  }
 
-  @objc
-  func setPushToken(tokenData: Data) {
-    KlaviyoSDK().setPushToken(tokenData)
-  }
+    @objc
+    static func setExternalId(_ externalId: String) {
+        KlaviyoSDK.setExternalId(externalId)
+    }
 
-  @objc
-  class func setPushTokenNative(tokenData: Data) {
-    KlaviyoSDK().setPushToken(tokenData)
-  }
+    @objc
+    static func getExternalId(_ callback: @escaping RCTResponseSenderBlock) {
+        if let externalId = KlaviyoSDK.getExternalId() {
+            callback([NSNull(), externalId])
+        } else {
+            callback(["External ID not found", NSNull()])
+        }
+    }
+
+    @objc
+    static func setPushToken(_ tokenData: Data) {
+        KlaviyoSDK.setPushToken(tokenData)
+    }
 }
